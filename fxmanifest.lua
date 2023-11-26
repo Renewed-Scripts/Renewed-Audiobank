e851eb4341494deb405d8594835e18d53d57f9aa
FjamZoo <laurids112@live.dk>
2023-11-26 19:55:31 +0100

    intial push

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Renewed Audio Bank'
+author 'FjamZoo - Renewed Scripts'
+version '1.0.0'
+
+files {
+    'data/audioexample_sounds.dat54.rel',
+	'audiodirectory/custom_sounds.awc'
+}
+
+data_file 'AUDIO_WAVEPACK' 'audiodirectory'
+data_file 'AUDIO_SOUNDDATA' 'data/audioexample_sounds.dat'
